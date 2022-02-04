c130789f07d88ab5b798da4d1032691b0d3410ed
hoehlrich <89951187+hoehlrich@users.noreply.github.com>
2022-02-03 19:53:41 -0500

    created vital classes
    - Created and developed WordleLetter
    - Created WordleWord
    - Test calculated scores for uraei and soare

--- main.py
+++ main.py
@@ -1,63 +1,120 @@
 # !/usr/bin/env
 # -*- coding: utf-8 -*-
 
-import json
 import string
 
-def load_json(path):
-    with open(path, 'r') as read_file:
-        return dict(json.load(read_file))
+from json_helpers import *
 
-def guess_total_points(guess, answers):
-    points = 0
+class WordleLetter():
+    '''WordleLetter Class'''
 
-    for answer in answers:
-        points += guess_points(guess, answer)
+    letters = []
+    answers = []
+
+    def __init__(self, letter):
+        self.letter = letter
+        WordleLetter.letters.append(self)
     
-    return points
+    def calculate_frequencies(self):
+        self.frequencies = {
+                            'total': 0,
+                            0: 0,
+                            1: 0,
+                            2: 0,
+                            3: 0,
+                            4: 0
+                        }
 
-def guess_points(guess, answer):
-    points = {
-        'gv': 6,
-        'gc': 5,
-        'yv': 4,
-        'yc': 3
-    }
-
-    vowels = ('a', 'e', 'i', 'o', 'u')
-
-    score = 0
-
-    # Yellow pass
-    yellowed_letters = []
-    for i, letter in enumerate(guess):
-        if letter in answer and letter not in yellowed_letters:
-            if letter != answer[i]: # If its not green
-                yellowed_letters.append(letter)
-                if letter in vowels:
-                    score += points['yv']
-                else:
-                    score += points['yc']
-    
-    # Green pass
-    for i, letter in enumerate(guess):
-        if letter == answer[i]:
-            if letter in vowels:
-                score += points['gv']
-            else:
-                score += points['gc']
-
-    return score
+        for answer in WordleLetter.answers:
+            for i in range(5):
+                try:
+                    index = answer.index(self.letter)
+                    self.frequencies[index] += 1
+                    self.frequencies['total'] += 1
+                except ValueError:
+                    break
                 
+    def __str__(self):
+        return f'{self.letter}: {self.frequencies}'
+
+    @classmethod
+    def get_letter(self, letter_str):
+        for letter in WordleLetter.letters:
+            if letter.letter == letter_str:
+                return letter
+
+    @classmethod
+    def set_answers_list(cls, answers):
+        WordleLetter.answers = answers
+
+    @classmethod
+    def generate_letter_data(cls, output_path=None):
+        if len(cls.letters) == 0:
+            wordle_letters = {}
+
+            # Calculate letter frequencies
+            for letter in string.ascii_lowercase:
+                wordle_letter = cls(letter)
+                wordle_letter.calculate_frequencies()
+                wordle_letters[letter] = wordle_letter
+
+            cls.sort_letters()
+
+            try:
+                write_data(output_path, {letter.letter: letter.frequencies for letter in cls.letters}) # Write to file
+            except TypeError:
+                pass
+
+    @classmethod
+    def load_letter_data(cls, input_path):
+        if len(cls.letters) == 0:
+            letters = load_asset(input_path)
+
+            for name, frequencies in letters.items():
+                letter = WordleLetter(name)
+                letter.frequencies = frequencies
+
+    @classmethod
+    def sort_letters(cls):
+        cls.letters = [(letter, letter.frequencies['total']) for letter in cls.letters] # Change cls.letters to a tupled format in the form of (letter_obj, total_frequency)
+        cls.letters.sort(key=lambda x: x[1], reverse=True) # Sort the tuples by the second item (frequency) reversely (greatest to least)
+        cls.letters = [letter[0] for letter in cls.letters] # Change cls.letters back to an object format
+
+class WordleWord():
+    '''WordleWord Class'''
+
+    words = []
+
+    def __init__(self, word):
+        self.word = word
+        WordleWord.words.append(self)
+    
+    def calculate_score(self):
+        score = 0
+        
+        for i, letter in enumerate(self.word):
+            letter_obj = WordleLetter.get_letter(letter)
+            score += letter_obj.frequencies['total']
+            score += letter_obj.frequencies[str(i)]
+        
+        self.score = score
+
+        return score
 
 def main():
-    # Load answers
-    answers = load_json('Assets/answers.json')['data']
+    WordleLetter.set_answers_list(load_asset('Assets/answers.json')['data']) # Set answers list for letters
     
-    soare_points = guess_total_points('soare', answers)
-    uraei_points = guess_total_points('uraei', answers)
+    # WordleLetter.generate_letter_data('Assets/letter_data.json') # Generate letter data
 
-    print(soare_points, uraei_points)
+    WordleLetter.load_letter_data('Assets/letter_data.json') # Load letter data
+
+    soare = WordleWord('soare')
+    soare.calculate_score()
+    print(soare.score)
+
+    uraei = WordleWord('uraei')
+    uraei.calculate_score()
+    print(uraei.score)
 
 if __name__ == '__main__':
     main()
